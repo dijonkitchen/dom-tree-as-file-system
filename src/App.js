54ee0bcc11d1f23438131dfaab0f3a8eeb3140ad
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-03-04 20:46:31 -0800

    Feat: Grab head and body from URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onReady(request) {
+    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        const doc = request.response.cloneNode(true)
+        console.log(doc)
+        const head = doc.head
+        const body = doc.body
+        console.log(head)
+        console.log(body)
+    }
+  }
+
+  requestInfo() {
+    const request = new XMLHttpRequest()
+    const CORSproxy = 'https://cors-anywhere.herokuapp.com/'
+    let link = CORSproxy + 'https://example.com'
+
+    request.open('GET', link)
+    request.responseType = 'document'
+    request.onreadystatechange = () => this.onReady(request)
+    request.send()
+  }
+
   render() {
+    this.requestInfo()
+
     return (
       <div className="App">
         <header className="App-header">
